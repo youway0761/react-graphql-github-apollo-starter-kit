3b1edf53f4cf5b54654c38291e98b50a3c988f4a
youway0761 <youway@hotmail.com>
2022-04-22 15:49:41 +0800

    ok

---
+++ src/Comment/CommentItem/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './style.css';
+
+const Comment = ({ comment }) => {
+    console.log({ comment });
+    return (
+        <div className="CommentItem">
+            <div>{comment.author.login}:</div>
+            &nbsp;
+            <div dangerouslySetInnerHTML={{ __html: comment.bodyHTML }} />
+        </div>
+    );
+};
+
+export default Comment;
